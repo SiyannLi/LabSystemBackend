add114c2f131a2f25ac5db13f80e6d19e0c96a50
Siyan <Asdlkj987>
2021-11-25 23:59:03 +0100

    wrote some tests api, that need no database

--- src/main/java/com/example/SpringbootTest/controller/UserController.java
+++ src/main/java/com/example/SpringbootTest/controller/UserController.java
@@ -12,6 +12,7 @@ import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -23,7 +24,7 @@ public class UserController {
 
     @ApiOperation("get one user")
     @GetMapping("get/{userId}")
-    public Result getUsers(@ApiParam(name = "userId", value = "userId", required = true) @PathVariable int userId) {
+    public Result<Object> getUser(@ApiParam(name = "userId", value = "userId", required = true) @PathVariable int userId) {
         User user = userService.getUser(userId);
         if (null != user) {
             return ResultGenerator.genSuccessResult(user);
@@ -32,9 +33,18 @@ public class UserController {
         }
     }
 
+    @ApiOperation("get one test user")
+    @GetMapping("getTestUser")
+    //不需要用到数据库
+    public Result<Object> getUserTest() {
+        User user = DataGenerate.generateUser();
+        return ResultGenerator.genSuccessResult(user);
+
+    }
+
     @ApiOperation("get all users")
     @GetMapping("getAll")
-    public Result getAll() {
+    public Result<Object> getAll() {
         List<User> users = userService.getAllUser();
         if (!users.isEmpty()) {
             return ResultGenerator.genSuccessResult(users);
@@ -44,14 +54,33 @@ public class UserController {
         }
     }
 
+    @ApiOperation("get all test users")
+    @GetMapping("getAllTest")
+    //不需要用到数据库
+    public Result<Object> getAllTest() {
+        List<User> users = new ArrayList<>();
+        users.add(DataGenerate.generateUser());
+        users.add(DataGenerate.generateUser());
+        users.add(DataGenerate.generateUser());
+
+        return ResultGenerator.genSuccessResult(users);
+    }
+
     @ApiOperation("insert one user")
     @PostMapping("/insertUser")
-    public Result insertUser(@ApiParam(name = "user", value = "user", required = true) @Param("user") @RequestBody User user) {
+    public Result<Object> insertUser(@ApiParam(name = "user", value = "user", required = true) @Param("user") @RequestBody User user) {
         if (userService.insertUser(user) > 0) {
             return ResultGenerator.genSuccessResult();
         } else {
             return ResultGenerator.genFailResult("fail to insert user");
         }
+    }
+
+    @ApiOperation("insert one user")
+    @PostMapping("/insertUserTest")
+    //不需要用到数据库
+    public Result<Object> insertUserTest(@ApiParam(name = "user", value = "user", required = true) @Param("user") @RequestBody User user) {
+        return ResultGenerator.genSuccessResult(user);
     }
 
 
