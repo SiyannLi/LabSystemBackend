d5c88d79c12ac2d695b776fa46682d926ed25750
Cong Liu <cong.liu@outlook.de>
2022-03-01 16:07:14 +0100

    user test

---
+++ src/test/java/com/example/LabSystemBackend/dao/UserDaoTest.java
@@ -0,0 +1,206 @@
+package com.example.LabSystemBackend.dao;
+
+import com.example.LabSystemBackend.entity.User;
+import com.example.LabSystemBackend.entity.UserAccountStatus;
+import com.example.LabSystemBackend.entity.UserRole;
+import com.example.LabSystemBackend.util.DataGenerate;
+import com.github.javafaker.Faker;
+import org.junit.Assert;
+import org.junit.jupiter.api.*;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.transaction.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.*;
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+@Rollback(value = true)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class UserDaoTest {
+
+    private String[] fullNames;
+    private String[] emails;
+    private UserRole[] roles;
+    private UserAccountStatus[] status;
+    private boolean[] isReceiveRequests;
+
+
+    @Autowired
+    UserDao userDao;
+
+    @BeforeAll
+    public void initArray() {
+        fullNames = new String[]{"TECO TECO", "Cong Liu", "Siyan Li", "Cong Liu"};
+        emails = new String[]{"teco@teco.com"
+                , "cong.liu@outlook.de"
+                , "siyannLi@outlook.com"
+                , "fraucong@gmail.com"};
+        roles = new UserRole[]{UserRole.SUPER_ADMIN, UserRole.USER, UserRole.ADMIN, UserRole.USER};
+        status = new UserAccountStatus[]{UserAccountStatus.ACTIVE, UserAccountStatus.PENDING
+                , UserAccountStatus.ACTIVE, UserAccountStatus.INACTIVE};
+        isReceiveRequests = new boolean[]{false, false, false, false};
+        Assert.assertEquals("Array init error", fullNames.length, emails.length, roles.length);
+        Assert.assertEquals("Array init error", fullNames.length, status.length, isReceiveRequests.length);
+    }
+
+    private void assertAllDataInList(List<User> users, List<Integer> idx) {
+        Assert.assertEquals(idx.size(), users.size());
+        for(int i = 0; i < users.size() && idx.contains(i); i++) {
+            Assert.assertEquals("user's data incorrect", users.get(i).getFullName(), fullNames[i]);
+            Assert.assertEquals("user's data incorrect", users.get(i).getEmail(), emails[i]);
+            Assert.assertEquals("user's data incorrect", users.get(i).getUserRole(), roles[i]);
+            Assert.assertEquals("user's data incorrect", users.get(i).getUserAccountStatus(), status[i]);
+            Assert.assertEquals("user's data incorrect", users.get(i).isReceiveBulkEmail(), isReceiveRequests[i]);
+        }
+    }
+
+    private void assertAllDataUser(User user) {
+        Assert.assertTrue(user.getUserId() <= fullNames.length);
+        int idx = user.getUserId() - 1;
+        Assert.assertEquals("user's data incorrect", user.getFullName(), fullNames[idx]);
+        Assert.assertEquals("user's data incorrect", user.getEmail(), emails[idx]);
+        Assert.assertEquals("user's data incorrect", user.getUserRole(), roles[idx]);
+        Assert.assertEquals("user's data incorrect", user.getUserAccountStatus(), status[idx]);
+        Assert.assertEquals("user's data incorrect", user.isReceiveBulkEmail(), isReceiveRequests[idx]);
+
+
+    }
+
+    @Test
+    void getAllUsers() {
+        List<User> users = userDao.getAllUsers();
+        List<Integer> notPendingIdx = new ArrayList<>();
+        for(int i = 0; i < fullNames.length; i++) {
+            if(!status[i].equals(UserAccountStatus.PENDING)) {
+                notPendingIdx.add(i);
+            }
+        }
+        // Does not include users whose status is Pending
+        assertAllDataInList(users, notPendingIdx);
+
+    }
+
+    @Test
+    void getAllAdministrators() {
+        List<Integer> adminIdx = new ArrayList<>();
+        for(int i = 0; i < fullNames.length; i++) {
+            if(!roles[i].equals(UserRole.USER)) {
+                adminIdx.add(i);
+            }
+        }
+        List<User> admins = userDao.getAllAdministrators();
+        assertAllDataInList(admins, adminIdx);
+
+
+    }
+
+    @Test
+    void getAllAccountToBeConfirmed() {
+        List<Integer> pendingIdx = new ArrayList<>();
+        for(int i = 0; i < fullNames.length; i++) {
+            if(status[i].equals(UserAccountStatus.PENDING)) {
+                pendingIdx.add(i);
+            }
+        }
+        List<User> users = userDao.getAllAccountToBeConfirmed();
+        assertAllDataInList(users,pendingIdx);
+    }
+
+    @Test
+    void getAllAdminReceiveBulkEmail() {
+        List<Integer> receiveAdminIdx = new ArrayList<>();
+        for(int i = 0; i < fullNames.length; i++) {
+            if(!roles[i].equals(UserRole.USER) && isReceiveRequests[i]) {
+                receiveAdminIdx.add(i);
+            }
+        }
+        List<User> users = userDao.getAllAdminReceiveBulkEmail();
+       assertAllDataInList(users, receiveAdminIdx);
+
+    }
+
+    @Test
+    void getUser() {
+        int id = new Random().nextInt(fullNames.length) + 1;
+        User user = userDao.getUser(id);
+        assertAllDataUser(user);
+    }
+
+    @Test
+    void getUserByEmail() {
+        int idx = new Random().nextInt(fullNames.length);
+        String email = emails[idx];
+        User user = userDao.getUserByEmail(email);
+        assertAllDataUser(user);
+    }
+
+    @Test
+    void deleteUser() {
+        int id = new Random().nextInt(fullNames.length) + 1;
+        Assert.assertTrue("fail to delete", 1 == userDao.deleteUser(id));
+
+    }
+
+    @Test
+    void insertUser() {
+        User newUser = DataGenerate.generateUser();
+        userDao.insertUser(newUser);
+        Assert.assertTrue("Fail to insert", fullNames.length + 1 == newUser.getUserId());
+
+    }
+
+    @Test
+    void updatePassword() {
+        int id = new Random().nextInt(fullNames.length) + 1;
+        Faker faker  = new Faker();
+        String newPassword = faker.internet().password();
+        Assert.assertTrue("fail to update password", 1 == userDao.updatePassword(id, newPassword));
+
+    }
+
+    @Test
+    void updateUserAccountStatus() {
+        int id = new Random().nextInt(fullNames.length) + 1;
+        int status = new Random().nextInt(UserAccountStatus.values().length);
+        UserAccountStatus newStatus = UserAccountStatus.values()[status];
+        Assert.assertTrue("fail to update status", 1 == userDao.updateUserAccountStatus(id, newStatus));
+    }
+
+    @Test
+    void updateName() {
+        int id = new Random().nextInt(fullNames.length) + 1;
+        Faker faker  = new Faker();
+        String[] name;
+        do {
+            name = faker.harryPotter().character().toLowerCase().split(" ");
+
+        } while (name.length != 2);
+        Assert.assertTrue("fail to update name", 1 == userDao.updateName(id, name[0], name[1]));
+    }
+
+    @Test
+    void updateUserRole() {
+        int id = new Random().nextInt(fullNames.length) + 1;
+        int role = new Random().nextInt(UserRole.values().length);
+        UserRole newRole = UserRole.values()[role];
+        Assert.assertTrue("fail to update role", 1 == userDao.updateUserRole(id, newRole));
+    }
+
+    @Test
+    void updateAdminEmailSetting() {
+        int id = new Random().nextInt(fullNames.length) + 1;
+        boolean stetting = new Random().nextInt(2) == 1 ? true : false;
+        Assert.assertTrue("fail to update email setting", 1 == userDao.updateAdminEmailSetting(id, stetting));
+
+    }
+}
---
+++ src/test/java/com/example/LabSystemBackend/jwt/AuthenticationInterceptorTest.java
@@ -0,0 +1,297 @@
+package com.example.LabSystemBackend.jwt;
+
+import com.auth0.jwt.exceptions.JWTDecodeException;
+import com.example.LabSystemBackend.common.ResponseGenerator;
+import com.example.LabSystemBackend.controller.UserController;
+import com.example.LabSystemBackend.entity.User;
+import com.example.LabSystemBackend.entity.UserAccountStatus;
+import com.example.LabSystemBackend.entity.UserRole;
+import com.example.LabSystemBackend.service.NotificationService;
+import com.example.LabSystemBackend.service.UserService;
+import com.example.LabSystemBackend.service.VerifyCodeService;
+import com.example.LabSystemBackend.ui.ExceptionMessage;
+import com.example.LabSystemBackend.ui.InputMessage;
+import com.example.LabSystemBackend.ui.KeyMessage;
+import com.example.LabSystemBackend.ui.OutputMessage;
+import com.example.LabSystemBackend.util.DataGenerate;
+import com.github.javafaker.Faker;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(UserController.class)
+class AuthenticationInterceptorTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private UserService userService;
+    @MockBean
+    private NotificationService notificationService;
+    @MockBean
+    private VerifyCodeService verifyCodeService;
+    //pass token
+    private final String passTokenTestUrl = "/users/visitorLogin";
+    private final String needTokenUrl = "/users/logout";
+    private final String adminTokenUrl = "/users/getAllAccountToBeConfirmed";
+    private final String superAdminTokenUrl = "/users/getAllAdministrator";
+
+    private void checkTokenPermission(String url, User user, int expectedCode, String expectMessage) {
+        String token = JwtUtil.createToken(user);
+        UserController.emailTokens.put(user.getEmail(), token);
+        try {
+            Mockito.when(userService.emailExists(user.getEmail())).thenReturn(true);
+            Mockito.when(userService.getUserByEmail(user.getEmail())).thenReturn(user);
+            MvcResult mvcResult = mockMvc.perform(
+                            MockMvcRequestBuilders.get(url)
+                                    .header(KeyMessage.TOKEN, token)
+                                    .accept(MediaType.APPLICATION_JSON)
+                    )
+                    .andExpect(MockMvcResultMatchers.jsonPath("$.resultCode").value(expectedCode))
+                    .andExpect(MockMvcResultMatchers.jsonPath("$.message").value(expectMessage))
+                    .andDo(MockMvcResultHandlers.print())
+                    .andReturn();
+            UserController.emailTokens.remove(user.getEmail());
+        } catch (Exception e) {
+            Assert.fail("fail to request");
+        }
+
+    }
+
+    @Test
+    void checkTokenAdminFail() {
+        User user = DataGenerate.generateUser();
+        int id = new Random().nextInt(100) + 1;
+        user.setUserId(id);
+        //set role
+        user.setUserRole(UserRole.USER);
+        checkTokenPermission(adminTokenUrl, user, 402, ExceptionMessage.NOT_ADMIN);
+    }
+
+    @Test
+    void checkTokenSuperAdminSucceed() {
+        User user = DataGenerate.generateUser();
+        int id = new Random().nextInt(100) + 1;
+        user.setUserId(id);
+        //set role
+        user.setUserRole(UserRole.SUPER_ADMIN);
+        user.setEmail(InputMessage.SUPER_ADMIN_EMAIL);
+        List<User> users = new ArrayList<>();
+        Mockito.when(userService.getAllAdministrators()).thenReturn(users);
+        checkTokenPermission(superAdminTokenUrl, user, 200, "SUCCESS");
+    }
+
+    @Test
+    void checkTokenSuperAdminFail() {
+        User user = DataGenerate.generateUser();
+        int id = new Random().nextInt(100) + 1;
+        user.setUserId(id);
+        UserRole role = Arrays.stream(UserRole.values()).filter(e -> !e.equals(UserRole.SUPER_ADMIN)).findAny().get();
+        //set role
+        user.setUserRole(role);
+        //user or admin
+        checkTokenPermission(superAdminTokenUrl, user, 402, ExceptionMessage.NOT_SUPER_ADMIN);
+    }
+
+    @Test
+    void checkTokenAdminSucceed() {
+        User user = DataGenerate.generateUser();
+        int id = new Random().nextInt(100) + 1;
+        user.setUserId(id);
+        //admin or super admin
+        UserRole role = Arrays.stream(UserRole.values()).filter(e -> !e.equals(UserRole.USER)).findAny().get();
+        user.setUserRole(role);
+        List<User> users = new ArrayList<>();
+        Mockito.when(userService.getAllAccountToBeConfirmed()).thenReturn(users);
+        checkTokenPermission(adminTokenUrl, user, 200, OutputMessage.NO_USER_TO_CONFIRM);
+    }
+
+    @Test
+    void nullTokenFail() {
+        try {
+            MvcResult mvcResult = mockMvc.perform(
+                            MockMvcRequestBuilders.get(needTokenUrl)
+                                    .accept(MediaType.APPLICATION_JSON)
+                    )
+                    .andExpect(MockMvcResultMatchers.jsonPath("$.resultCode").value(402))
+                    .andExpect(MockMvcResultMatchers.jsonPath("$.message").value(ExceptionMessage.TOKEN_NULL))
+                    .andDo(MockMvcResultHandlers.print())
+                    .andReturn();
+        } catch (Exception e) {
+            Assert.fail("fail to request");
+        }
+    }
+
+    @Test
+    void tokenWorryFail() {
+        try {
+            Faker faker = new Faker();
+            String token = faker.internet().password();
+            MvcResult mvcResult = mockMvc.perform(
+                            MockMvcRequestBuilders.get(needTokenUrl)
+                                    .header(KeyMessage.TOKEN, token)
+                                    .accept(MediaType.APPLICATION_JSON)
+                    )
+                    .andExpect(MockMvcResultMatchers.jsonPath("$.resultCode").value(402))
+                    .andExpect(MockMvcResultMatchers.jsonPath("$.message").value(ExceptionMessage.TOKEN_WRONG))
+                    .andDo(MockMvcResultHandlers.print())
+                    .andReturn();
+        } catch (Exception e) {
+            Assert.fail("fail to request");
+        }
+    }
+
+    @Test
+    void tokenConflictFail() {
+        try {
+            User user = DataGenerate.generateUser();
+            int id = new Random().nextInt(100) + 1;
+            user.setUserId(id);
+            String tokenFront = JwtUtil.createToken(user);
+            String tokenServer = JwtUtil.createToken(user, 2, 1);
+            UserController.emailTokens.put(user.getEmail(), tokenServer);
+            MvcResult mvcResult = mockMvc.perform(
+                            MockMvcRequestBuilders.get(needTokenUrl)
+                                    .header(KeyMessage.TOKEN, tokenFront)
+                                    .accept(MediaType.APPLICATION_JSON)
+                    )
+                    .andExpect(MockMvcResultMatchers.jsonPath("$.resultCode").value(402))
+                    .andExpect(MockMvcResultMatchers.jsonPath("$.message").value(ExceptionMessage.TOKEN_CONFLICT))
+                    .andDo(MockMvcResultHandlers.print())
+                    .andReturn();
+            UserController.emailTokens.remove(user.getEmail());
+        } catch (Exception e) {
+            Assert.fail("fail to request");
+        }
+    }
+
+    @Test
+    void emailNotExistFail() {
+        try {
+            User user = DataGenerate.generateUser();
+            int id = new Random().nextInt(100) + 1;
+            user.setUserId(id);
+            String token = JwtUtil.createToken(user);
+            UserController.emailTokens.put(user.getEmail(), token);
+            Mockito.when(userService.emailExists(user.getEmail())).thenReturn(false);
+            MvcResult mvcResult = mockMvc.perform(
+                            MockMvcRequestBuilders.get(needTokenUrl)
+                                    .header(KeyMessage.TOKEN, token)
+                                    .accept(MediaType.APPLICATION_JSON)
+                    )
+                    .andExpect(MockMvcResultMatchers.jsonPath("$.resultCode").value(402))
+                    .andExpect(MockMvcResultMatchers.jsonPath("$.message").value(ExceptionMessage.USER_NOT_EXISTS))
+                    .andDo(MockMvcResultHandlers.print())
+                    .andReturn();
+            UserController.emailTokens.remove(user.getEmail());
+        } catch (Exception e) {
+            Assert.fail("fail to request");
+        }
+    }
+
+    @Test
+    void tokenExpiredFail() {
+        try {
+            User user = DataGenerate.generateUser();
+            int id = new Random().nextInt(100) + 1;
+            user.setUserId(id);
+            String token = JwtUtil.createToken(user, 2, 1);
+            UserController.emailTokens.put(user.getEmail(), token);
+            Thread.currentThread().sleep(2000);
+            Mockito.when(userService.emailExists(user.getEmail())).thenReturn(true);
+            MvcResult mvcResult = mockMvc.perform(
+                            MockMvcRequestBuilders.get(needTokenUrl)
+                                    .header(KeyMessage.TOKEN, token)
+                                    .accept(MediaType.APPLICATION_JSON)
+                    )
+                    .andExpect(MockMvcResultMatchers.jsonPath("$.resultCode").value(402))
+                    .andExpect(MockMvcResultMatchers.jsonPath("$.message").value(ExceptionMessage.EXPIRED_TOKEN))
+                    .andDo(MockMvcResultHandlers.print())
+                    .andReturn();
+            UserController.emailTokens.remove(user.getEmail());
+        } catch (Exception e) {
+            Assert.fail("fail to request");
+        }
+    }
+
+    @Test
+    void CheckTokenRefresh() {
+        try {
+            User user = DataGenerate.generateUser();
+            int id = new Random().nextInt(100) + 1;
+            user.setUserId(id);
+            String token = JwtUtil.createToken(user, 2, 1);
+            UserController.emailTokens.put(user.getEmail(), token);
+            Thread.currentThread().sleep(1000);
+            Mockito.when(userService.emailExists(user.getEmail())).thenReturn(true);
+            Mockito.when(userService.getUserByEmail(user.getEmail())).thenReturn(user);
+            MvcResult mvcResult = mockMvc.perform(
+                            MockMvcRequestBuilders.get(needTokenUrl)
+                                    .header(KeyMessage.TOKEN, token)
+                                    .accept(MediaType.APPLICATION_JSON)
+                    )
+                    .andExpect(MockMvcResultMatchers.jsonPath("$.resultCode").value(200))
+                    .andExpect(MockMvcResultMatchers.jsonPath("$.message").value(OutputMessage.SUCCEED))
+                    .andDo(MockMvcResultHandlers.print())
+                    .andReturn();
+            String tokenServer = UserController.emailTokens.get(user.getEmail());
+            Assert.assertNotEquals(token, tokenServer);
+            UserController.emailTokens.remove(user.getEmail());
+        } catch (Exception e) {
+            Assert.fail("fail to request");
+        }
+    }
+
+    @Test
+    void checkForPassToken() {
+        Faker faker = new Faker();
+        String token = faker.internet().password();
+        User user = DataGenerate.generateUser();
+        int id = new Random().nextInt(100) + 1;
+        user.setUserId(id);
+        user.setUserAccountStatus(UserAccountStatus.ACTIVE);
+        Mockito.when(userService.emailExists(user.getEmail())).thenReturn(true);
+        Mockito.when(userService.getUserByEmail(user.getEmail())).thenReturn(user);
+        String body = "{\"" + KeyMessage.EMAIL + "\":\"" + user.getEmail() + "\",\"" + KeyMessage.PASSWORD
+                + "\":\"" + user.getUserPassword() + "\"}";
+        try {
+            MvcResult mvcResult = mockMvc.perform(
+                            MockMvcRequestBuilders.post(passTokenTestUrl)
+                                    .header(KeyMessage.TOKEN, token)
+                                    .contentType(MediaType.APPLICATION_JSON).content(body)
+                                    .accept(MediaType.APPLICATION_JSON)
+                    )
+                    .andExpect(MockMvcResultMatchers.jsonPath("$.resultCode").value(200))
+                    .andExpect(MockMvcResultMatchers.jsonPath("$.token").exists())
+                    .andExpect(MockMvcResultMatchers.jsonPath("$.email").value(user.getEmail()))
+                    .andExpect(MockMvcResultMatchers.jsonPath("$.firstName").value(user.getFirstName()))
+                    .andExpect(MockMvcResultMatchers.jsonPath("$.lastName").value(user.getLastName()))
+                    .andExpect(MockMvcResultMatchers.jsonPath("$.userLogged").value(true))
+                    .andExpect(MockMvcResultMatchers.jsonPath("$.adminLogged").value(false))
+                    .andDo(MockMvcResultHandlers.print())
+                    .andReturn();
+        } catch (Exception e) {
+            Assert.fail("fail to request");
+        }
+        UserController.emailTokens.remove(user.getEmail());
+
+    }
+}
